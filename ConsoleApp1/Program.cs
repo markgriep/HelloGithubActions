0083e67bdbc42508020f7d96d0c56cacc2761158
megriep <99667134+megriep@users.noreply.github.com>
2022-09-06 17:24:48 -0500

    Add solution to the folder with a one simple method

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine($" add two numbers = {AddTwoNumbers(1, 1)}");
+            Console.ReadKey();
+        }
+
+
+        public static int AddTwoNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
